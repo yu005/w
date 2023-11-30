5c99e8f6d5dd7140cc03ac761d352c46e9f67e59
yu005 <146842778+yu005@users.noreply.github.com>
2023-11-30 17:05:30 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace s1122100_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(num[0]);
+            int b = Convert.ToInt32(num[1]);
+            int c = Convert.ToInt32(num[2]);
+            Triangle A = new Triangle(a, b, c);
+            if(A.IsTriangle())
+            {
+                Console.WriteLine("Yes");
+
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+    }
+    class Triangle
+    {
+        private int a;
+        private int b;
+        private int c;
+        public Triangle(int a,int b,int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void SetEdges(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+        }
+        public bool IsTriangle()
+        {
+            if (a + b > c && b + c > a && a + c > b)
+            {
+              return true;
+            }
+                
+            else
+            {
+                return false;
+            }
+
+            
+        }
+    }
+}
