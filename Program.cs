db0e5731b79a1e6f7b588abb650340d0ccb2e214
yu005 <146842778+yu005@users.noreply.github.com>
2023-12-07 17:21:08 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1207_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int N = input.Length;
+            int M = (int)Math.Ceiling(Math.Sqrt(N));
+
+            char[,] c = new char[M, M];
+            //String[] b = input.Split(""); 
+            int index = 0;
+            //String[] b = new string[N];
+
+
+            for (int i =0;i<M;i++)
+            {
+                for(int j=0;j<M;j++)
+                {
+                    if (index<N)
+                    {
+                        c[i,j] = input[index];
+                    }
+                    else
+                    {
+                        c[i, j] =' ';
+                    }
+                        index++;
+                }
+            }
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(c[j, i]);
+
+                }
+            }
+
+
+        }
+    }
+}
