4a6b30169ea9fd973e07f7f18be841b305578a67
yu005 <146842778+yu005@users.noreply.github.com>
2023-12-13 11:34:23 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _1213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split("");
+            string []K = Console.ReadLine().Split(' ');
+            int x = Convert.ToInt32(K[0]);
+            int y = Convert.ToInt32(K[1]);
+            char[,] arr = new char[x, y];
+            if (arr[x,y]== 1)
+                FinfLake(x,y);
+
+
+        }
+        static int[,] arr;
+        static int c;
+        
+        static void FinfLake(int i,int j)
+        {
+            
+            c += 1;
+            arr[i, j] = 0;
+            if(arr[i-1,j]==1)//左1
+            {
+                FinfLake(i - 1, j);
+                c++;
+                arr[i - 1, j] = 0;
+            }
+            if (arr[i +1, j] == 1)//右1
+            {
+                FinfLake(i + 1, j);
+                c++;
+                arr[i + 1, j] = 0;
+            }
+            if (arr[i , j-1] == 1)//上1
+            {
+                FinfLake(i , j-1);
+                c++;
+                arr[i , j-1] = 0;
+            }
+            if (arr[i , j+1] == 1)//下1
+            {
+                FinfLake(i , j+1);
+                c++;
+                arr[i , j+1] = 0;
+            }
+        }
+    }
+}
