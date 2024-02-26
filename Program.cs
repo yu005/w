52d928e8d5cc195e896882b24a5bf583e91a2637
yu005 <146842778+yu005@users.noreply.github.com>
2024-02-26 21:33:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+            int a = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a += i;
+
+                }
+            }
+            if (n == a)
+            {
+                Console.WriteLine($"{n}: perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"{n}: normal number");
+            }
+        }
+    }
+}
