36db15d1dc5321baf4230105791d03c3b7f61979
yu005 <146842778+yu005@users.noreply.github.com>
2024-03-07 13:28:28 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ta1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            string[] nums = data.Split(' ');
+            int x = int.Parse(nums[0]);
+            if( (x % 4 == 0 && x % 100 != 0) || (x%400 == 0))
+            {
+                Console.WriteLine("閏年");
+            }
+            else
+            {
+            Console.WriteLine("平年");
+            }
+
+
+        }
+    }
+}
