d9b0593643f913092e99a2f455527da00dd03dfc
yu005 <146842778+yu005@users.noreply.github.com>
2024-03-14 01:32:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PB2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            
+
+            if (!IsLegal(str[0]) || !IsLegal(str[1]))
+            {
+                Console.WriteLine($"Wrong Input");
+            }
+
+
+
+        }
+        public static bool IsLegal(string guessNumber)
+        {
+
+            for (int i = 0; i < guessNumber.Length; i++)
+            {
+                for (int j = i + 1; j < guessNumber.Length; j++)
+                {
+                    if (guessNumber[i] == guessNumber[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (guessNumber.Length != 3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
