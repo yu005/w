bab5aa9820b9fb3f09ab96863092ed109959496e
yu005 <146842778+yu005@users.noreply.github.com>
2024-03-14 01:31:19 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace paIsLegal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            if (IsLegal(str))
+            {
+                Console.WriteLine($"{str} is legal.");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is illegal.");
+            }
+
+
+        }
+        public static bool IsLegal(string guessNumber)
+        {
+
+            for (int i = 0; i < guessNumber.Length; i++)
+            {
+                for (int j = i + 1; j < guessNumber.Length; j++)
+                {
+                    if (guessNumber[i] == guessNumber[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (guessNumber.Length != 3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
