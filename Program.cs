4b2e38bf17b6362b6ee79dc456c3c0e4dab2d571
yu005 <146842778+yu005@users.noreply.github.com>
2024-03-19 23:22:57 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,55 @@
-namespace s1122100ex0318Lab003
+﻿using System;
+namespace ta4_1
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            string str = Console.ReadLine();
+            int mon = Convert.ToInt32(str);
+            int day = 0;
+            switch (mon)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    day = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    day = 30;
+                    break;
+                case 2:
+                    day = 28;
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    return;
+            }
+            Console.WriteLine($"{mon} 月的好日子:");
+            for(int i = 1; i <= day; i++) 
+            {
+                if ( i % 4 != 0 && i % 10 != 4)
+                { 
+                    if(i==day)
+                    {
+                        Console.Write($"{i}");
+                    }
+                    else
+                    {
+                    Console.Write($"{i} ");
+                    }
+
+
+                }
+            }
+
         }
     }
 }
