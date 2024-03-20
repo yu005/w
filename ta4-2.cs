eca2ca00989880211ff9e3e21799438950391902
yu005 <146842778+yu005@users.noreply.github.com>
2024-03-21 01:43:38 +0800

    Update ta4-2.cs

--- ta4-2.cs
+++ ta4-2.cs
@@ -1,47 +1,51 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices.Marshalling;
-
+using System;
 namespace ta4_2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        {
             double sum = 0;
             int ans = 0;
-            Console.WriteLine("請輸入業績:(-1 for exit) ");
-            for (; ;)
-            { 
-            string str = Console.ReadLine();
-            int n = Convert.ToInt32(str);
 
-            if (n == -1)
-            {
-                break;
-            }
-            if (n > 0 && n < 100)
-            {
-                sum++;
-                ans += n;
-                    Console.WriteLine("請輸入業績:(-1 for exit) ");
-            }
-            else
-            {
-                Console.WriteLine("*** Error Data! ");
-            }
+            for (; ; )
+            {            
+                Console.WriteLine("請輸入業績:(-1 for exit)");
+                string str = Console.ReadLine();
+                int n = Convert.ToInt32(str);
+
+                if (n == -1)
+                {
+                    break;
+                }
+                if (n > 0 && n < 100)
+                {
+                    sum++;
+                    ans += n;
+ 
+                }
+                else
+                {
+                    Console.WriteLine("*** Error Data!");
+                }
             }
             double anss = ans / sum;
-            if(sum>0)
+                Console.WriteLine("======================");
+                Console.WriteLine($"員工人數:{sum}");
+            if (sum != 0)
             {
-                Console.WriteLine("====================== ");
-            Console.WriteLine($"員工人數:{sum} ");
-            Console.WriteLine($"平均業績:{anss:F2} ");
+
+                Console.WriteLine($"平均業績:{anss:F2}");
             }
-            else 
-            {
-                Console.WriteLine("====================== ");
-                Console.WriteLine("員工人數:0 ");
-            }
+
+
+
+
+        }
+    }
+} 
+
+
 
 
 
